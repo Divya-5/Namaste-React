92ca28cb750954e1acf371d71e1b6ce57fa8d398
Divyanshi Rastogi <43536638+Divya-5@users.noreply.github.com>
2023-01-10 16:25:35 +0530

    Inception

---
+++ App.js
@@ -0,0 +1,26 @@
+const headings = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Namaste React Everyone"
+);
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "heading2"
+);
+console.log(headings);
+const container = React.createElement(
+  "div",//tag-name
+  {
+    id: "container", //attribute in react called props
+  },
+  [headings, heading2]// children 
+);
+const roots = ReactDOM.createRoot(document.getElementById("root"));
+// passing a react Element inside the root
+//  roots.render(headings);
+roots.render(container);
