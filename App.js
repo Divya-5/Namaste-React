2667b85ba024640e5928131c13b91ef8db666f67
Divyanshi Rastogi <43536638+Divya-5@users.noreply.github.com>
2023-01-12 20:34:10 +0530

    jsx

--- App.js
+++ App.js
@@ -1,31 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const headings = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key:"h1",
-  },
-  "Namaste React Everyone"
-);
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-    key: "h2",
-  },
-  "heading2"
-);
-console.log(headings);
-const container = React.createElement(
-  "div",//tag-name
-  {
-    id: "container", //attribute in react called props
-  },
-  [headings, heading2]// children 
-);
+const heading2 = <h1>Namaste React</h1>;
+//both exactly same we can skip return but for multiple lines we need paranthesis but for single line we dont need paranthesis
+const HeaderComponentSingle = () => {
+  return  <div> <h1>Namaste React</h1></div>
+};
+const HeaderComponentMultiple = () => {
+  return (
+    <div>
+      <h1>Namaste React</h1> <h2>Namaste JS</h2>
+    </div>
+  );
+};
+const HeaderComponent2 = () => (
+    <div>
+      <h1>Namaste React</h1> <h2>Namaste JS</h2>
+    </div>
+  );
+
 const roots = ReactDOM.createRoot(document.getElementById("root"));
 // passing a react Element inside the root
 //  roots.render(headings);
-roots.render(container);
+//ASYNC DEFER
+roots.render(heading2);
